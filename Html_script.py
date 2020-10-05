0c1844840aa83cb16fa5fe1bc92c9a8ceb299c89
PO1NTblank <42290740+PO1NTblank@users.noreply.github.com>
2020-10-06 01:11:51 +0530

    Update Html_script.py

--- Html_script.py
+++ Html_script.py
@@ -112,11 +112,9 @@ def retrieve_html():
     for year in range(2013,2019):
         for month in range(1,13):
             if(month<10):
-                url='http://en.tutiempo.net/climate/0{}-{}/ws-421820.html'.format(month
-                                                                          ,year)
+                url='http://en.tutiempo.net/climate/0{}-{}/ws-421820.html'.format(month ,year)
             else:
-                url='http://en.tutiempo.net/climate/{}-{}/ws-421820.html'.format(month
-                                                                          ,year)
+                url='http://en.tutiempo.net/climate/{}-{}/ws-421820.html'.format(month ,year)
             texts=requests.get(url)
             text_utf=texts.text.encode('utf=8')
             
@@ -131,7 +129,7 @@ if __name__=="__main__":
     start_time=time.time()
     retrieve_html()
     stop_time=time.time()
-    print("Time taken {}".format(stop_time-start_time))
+    print("Time elapsed: ".format(stop_time-start_time))
     
         
     
